d1c8a28728c8e56c2bf9a1b570ea313855753064
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-04-04 16:53:39 +0200

    User input
    Creating a welcome message

--- src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
+++ src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
@@ -4,6 +4,13 @@ import javax.swing.JOptionPane;
 public class PROG5121_Part1 {
 
     public static void main(String[] args) {
+         
+        JOptionPane.showConfirmDialog(null, "Would you like to create an account? ");
+       
+        String firstName = JOptionPane.showInputDialog(null, "Enter your first name");
+        String lastName = JOptionPane.showInputDialog(null, "Enter your last name");
+             
+        JOptionPane.showMessageDialog(null, "Welcome " + firstName + lastName);
         
     }
 }
