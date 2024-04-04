409553953dc6a54bf82d30c18ba69121e8422b53
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-04-04 17:35:33 +0200

    Asked user if they would like to create an account
    Asked the user if they would like to create an account before creating a username and creating a password

--- src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
+++ src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
@@ -5,12 +5,21 @@ public class PROG5121_Part1 {
 
     public static void main(String[] args) {
          
-        JOptionPane.showConfirmDialog(null, "Would you like to create an account? ");
-       
         String firstName = JOptionPane.showInputDialog(null, "Enter your first name");
         String lastName = JOptionPane.showInputDialog(null, "Enter your last name");
-             
+        
         JOptionPane.showMessageDialog(null, "Welcome " + firstName + lastName);
         
+        JOptionPane.showConfirmDialog(null, "Would you like to create an account?" ,firstName + lastName, JOptionPane.YES_NO_OPTION);
+        
+        createUsername();
+        createPassword();
+    }
+    public static void createUsername(){
+    
+    }
+    public static void createPassword(){
+    
+    
     }
 }
