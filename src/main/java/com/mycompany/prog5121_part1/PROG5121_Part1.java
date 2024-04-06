51d32de51e325a8e3561515fb8d9d322fdee0363
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-04-06 18:01:51 +0200

    Fixed the code
    Made the code look more professional

--- src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
+++ src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
@@ -8,21 +8,21 @@ public class PROG5121_Part1 {
         String strfirstName = JOptionPane.showInputDialog(null, "Enter your first name: ");
         String strlastName = JOptionPane.showInputDialog(null, "Enter your last name: ");
         
-        JOptionPane.showMessageDialog(null,"Welcome " + strfirstName + strlastName, "Output" , JOptionPane.PLAIN_MESSAGE); 
+        JOptionPane.showMessageDialog(null,"Welcome " + strfirstName + " " + strlastName, "Output" , JOptionPane.PLAIN_MESSAGE); 
         
         int iUserResponse = JOptionPane.showConfirmDialog(null, "Would you like to create an account?" , "Confirm account creation",
                 JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);//Allow user to choose whether they want to create an account or not
         
         if(iUserResponse == JOptionPane.YES_OPTION){
         
-        JOptionPane.showMessageDialog(null, "You may proceed to create a Username and a Password.");//If they select yes they may proceed with creating the username and the password
+        JOptionPane.showMessageDialog(null, "You may proceed to create a Username and a Password.", "Output", JOptionPane.PLAIN_MESSAGE);//If they select yes they may proceed with creating the username and the password
         
         
         }
         else if(iUserResponse == JOptionPane.NO_OPTION)
         {
             
-        JOptionPane.showMessageDialog(null, "Thank you for using my app.");//If they select no thank the user for using my app
+        JOptionPane.showMessageDialog(null, "Thank you for using my app.", "Output", JOptionPane.PLAIN_MESSAGE);//If they select no thank the user for using my app
         
         }
         
