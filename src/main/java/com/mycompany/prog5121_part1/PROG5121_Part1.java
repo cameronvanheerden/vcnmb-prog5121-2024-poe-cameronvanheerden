5a78d4e44479badba78ecee1e1150a321ce85913
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-04-06 18:49:00 +0200

    Started figuring out the username
    Corrected a few errors and trying to figure out how to create the username

--- src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
+++ src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
@@ -8,29 +8,40 @@ public class PROG5121_Part1 {
         String strfirstName = JOptionPane.showInputDialog(null, "Enter your first name: ");
         String strlastName = JOptionPane.showInputDialog(null, "Enter your last name: ");
         
-        JOptionPane.showMessageDialog(null,"Welcome " + strfirstName + " " + strlastName, "Output" , JOptionPane.PLAIN_MESSAGE); 
+        JOptionPane.showMessageDialog(null,"Welcome " + strfirstName + " " + strlastName +"!", "Output", JOptionPane.PLAIN_MESSAGE); 
         
         int iUserResponse = JOptionPane.showConfirmDialog(null, "Would you like to create an account?" , "Confirm account creation",
                 JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);//Allow user to choose whether they want to create an account or not
         
         if(iUserResponse == JOptionPane.YES_OPTION){
         
-        JOptionPane.showMessageDialog(null, "You may proceed to create a Username and a Password.", "Output", JOptionPane.PLAIN_MESSAGE);//If they select yes they may proceed with creating the username and the password
+        JOptionPane.showMessageDialog(null, "You may proceed to create an account.", "Output", JOptionPane.PLAIN_MESSAGE);//If they select yes they may proceed with creating the username and the password
         
         
         }
         else if(iUserResponse == JOptionPane.NO_OPTION)
         {
             
-        JOptionPane.showMessageDialog(null, "Thank you for using my app.", "Output", JOptionPane.PLAIN_MESSAGE);//If they select no thank the user for using my app
+        JOptionPane.showMessageDialog(null, "Thank you for using my app.", "Output", JOptionPane.ERROR);//If they select no thank the user for using my app
         
         }
-        
+        //I am calling the create Username and create Password method within the main method
         createUsername();
         createPassword();
     }
     public static void createUsername(){
+    String strUserName = JOptionPane.showInputDialog(null, "Create a username"
+            + "\nThe username must contain an underscore & should not be longer than 5 characters.",
+            "Username Requirements", JOptionPane.INFORMATION_MESSAGE);
     
+    if(strUserName.length()>=5){
+    JOptionPane.showMessageDialog(null, "Username captured successfully.", " Success", JOptionPane.PLAIN_MESSAGE);
+    
+    }
+    else{
+    JOptionPane.showMessageDialog(null, "Username is not correctly formatted, please ensure that your username contains an underscore"
+            + " and is no more than 5 character in length", "Error", JOptionPane.ERROR_MESSAGE);
+    }
     }
     public static void createPassword(){
     
