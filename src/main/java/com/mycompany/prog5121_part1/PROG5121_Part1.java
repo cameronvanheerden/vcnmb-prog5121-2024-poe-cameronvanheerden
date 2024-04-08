e7898a2bda3891f4d097289b00d232d6e2e2415b
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-04-08 09:10:16 +0200

    Password requirements
    Completed the password requirements

--- src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
+++ src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
@@ -8,8 +8,6 @@ public class PROG5121_Part1 {
         String strfirstName = JOptionPane.showInputDialog(null, "Enter your first name: ");
         String strlastName = JOptionPane.showInputDialog(null, "Enter your last name: ");
         
-        JOptionPane.showMessageDialog(null,"Welcome " + strfirstName + " " + strlastName +"!", "Output", JOptionPane.PLAIN_MESSAGE); 
-        
         int iUserResponse = JOptionPane.showConfirmDialog(null, "Would you like to create an account?" , "Confirm account creation",
                 JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);//Allow user to choose whether they want to create an account or not
         
@@ -30,6 +28,8 @@ public class PROG5121_Part1 {
         //I am calling the create Username and create Password method within the main method
         createUsername();
         createPassword();
+        JOptionPane.showMessageDialog(null,"Welcome " + strfirstName + " " + strlastName + ", it is great to see you again!"
+                , "Output", JOptionPane.PLAIN_MESSAGE); 
     }
     public static void createUsername(){
         boolean correctUsername = true;//Checks if the username entered is correct
@@ -37,7 +37,7 @@ public class PROG5121_Part1 {
         {
             
     String strUserName = JOptionPane.showInputDialog(null, "Create a username"
-            + "\nThe username must contain an underscore & should not be longer than 5 characters.",
+            + "\n-The username must contain an underscore \n-Should not be longer than 5 characters.",
             "Username Requirements", JOptionPane.INFORMATION_MESSAGE);
     
     if(strUserName.contains("_")&& strUserName.length()<=5 )//If statement ensures that username contains a underscore and is not longer than 5 characters
@@ -56,7 +56,31 @@ public class PROG5121_Part1 {
     }
     
     public static void createPassword(){
+    boolean correctPassword = true;//Checks if the password is correct
+    while(correctPassword == true){//Start of loop and will loop until password entered is entered correctly
+    
+   String strPassword = JOptionPane.showInputDialog(null, "Create a password"
+           + "\n-Must be at least 8 Characters long"
+           + "\n-Contain a capital letter"
+           + "\n-Contain a number"
+           + "\n-Contain a special character"
+           , "Password Requirements",JOptionPane.INFORMATION_MESSAGE);//Password requirements
+           
+    if(strPassword.length()>=8 && strPassword.matches(".*[A-Z].*") && strPassword.matches(".*[0-9].*")&&
+            strPassword.matches(".*[!,@,#,%,&,*,~,.,$].*$")) //If statement checks if password entered meets the requirements
+    {
+    
+        JOptionPane.showMessageDialog(null, "Password successfully captured", "Success", JOptionPane.PLAIN_MESSAGE);
+    correctPassword = false;
+    }
+    
+    else{
+    JOptionPane.showMessageDialog(null, "Password is not correctly formatted, please ensure that the password contains"
+            + "at least 8 characters, a capital letters, a number and a special character.", "Error", JOptionPane.ERROR_MESSAGE);
+    
+    }
     
     
+      }//end loop
     }
 }
