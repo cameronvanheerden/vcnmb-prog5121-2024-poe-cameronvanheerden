ea2653dd75b8974de0da2f6caada1f1a8e5d72dc
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-04-11 15:48:20 +0200

    Fixed main class and fixed login class
    After seeing the error messages i got a fixed my main class by assigning a username and password to each method before creating an instance

--- src/main/java/com/mycompany/prog5121_part1/Login.java
+++ src/main/java/com/mycompany/prog5121_part1/Login.java
@@ -1,28 +1,38 @@
 package com.mycompany.prog5121_part1;
 
 public class Login {
-   private String Username;//Declaring the necessary String variables
-   private String Password;
+   private String Username1;//Declaring the necessary String variables
+   private String Password1;
+   private String Registration;
    
    private boolean validPassword; //Declaring Necessary boolean variables
    private boolean validUsername;
+   private String strUsername;
+   private String strPassword;
+    
+    public Login(String Username, String Password){
+    
+    this.Username1 = strUsername;   
+    this.Password1 = strPassword;
+    
+    }
    
-    public boolean checkUserName(){
+     public boolean checkUserName(String strUsername){
    
     validUsername= false;//Username before user input has been assign to false by default
         
-    if(Username.contains("_")&& Username.length()<=5 ){//The condition states that the username has to have an underscore and is not more than 5 characters long
+    if(strUsername.contains("_")&& strUsername.length()<=5 ){//The condition states that the username has to have an underscore and is not more than 5 characters long
     
     validUsername = true;//If user inputs their username according to the condition it will be true
     } 
     return validUsername;//Returns the whether the conditions have been met 
 }    
 
-    public boolean checkPasswordComplexity(){
+    public boolean checkPasswordComplexity(String strPassword){
     validPassword = false;//Password before user input is assigned to false by default
            
-    if(Password.length()>=8 && Password.matches(".*[A-Z].*") && Password.matches(".*[0-9].*")&&
-            Password.matches(".*[!,.,&,*,?,@,#,%,$].*")) //If statement checks if password entered meets the requirements
+    if(strPassword.length()>=8 && strPassword.matches(".*[A-Z].*") && strPassword.matches(".*[0-9].*")&&
+            strPassword.matches(".*[!,.,&,*,?,@,#,%,$].*")) //If statement checks if password entered meets the requirements
     {
     validPassword = true;//If user inputs their password according to the condition it will be true
     }  
@@ -53,13 +63,18 @@ public class Login {
     }
     }  
  
-  
-    public boolean loginUser(String inputUsername, String inputPassword){
-       return inputPassword.equals(Password) && inputUsername.equals(Username); //This if statement ensures that the login details entered matches the login in detaIls stored when the username and password strings
-       //If login status matches it returns
-       //If login status doesnt match it returns a false
-    }
+    public boolean loginUser(String Username, String Password){
+     if(Username.equals(strUsername) && Password.equals(strPassword)){ //This if statement ensures that the login details entered matches the login in detauls stored when the user re
+          
+      return true;//If login status matches it returns a true
+     }
+     else{
+         
+     return false;//If login status doesnt match it returns a false
+     }
     
+  }
+       
     public String returnLoginStatus(){
      if(validPassword && validUsername){//This if statement checks if the information that the user gave is correct
      return "Login Successful";//this statement will return if the login is correct
--- src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
+++ src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
@@ -1,10 +1,83 @@
 package com.mycompany.prog5121_part1;
 
 import javax.swing.JOptionPane;
-public class PROG5121_Part1 {
 
-    public static void main(String[] args) {
-      
+public class PROG5121_Part1 {
+    
+    public static void main(String[] args) {   
         
+      int iUserResponse = JOptionPane.showConfirmDialog(null, "Would you like to create an account?" , "Confirm",
+                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);//Allow user to choose whether they want to create an account or not
+        
+       if(iUserResponse == JOptionPane.YES_OPTION){
+
+     Name(); //Calling this method within the main method
+     createUsername();
+     createPassword();
+     
+     }
+      else if(iUserResponse == JOptionPane.NO_OPTION)
+     {
+            
+     JOptionPane.showMessageDialog(null, "Thank you for using my app.", "Output", JOptionPane.PLAIN_MESSAGE);
+      //If they select no thank the user for using my app
+        
+      System.exit(iUserResponse);//Exits the program without continuing 
+      
+      }
+     
     }
+     public static void Name(){
+         String firstName;
+         String lastName;
+         
+         firstName = JOptionPane.showInputDialog(null, "Enter your first name: ");
+         lastName = JOptionPane.showInputDialog(null, "Enter your last name: ");
+       
     }
+     
+    public static void createUsername(){
+    String Username1;
+    String Password1 = "";
+                       
+    Username1 = JOptionPane.showInputDialog(null, "Create a username"
+                + "\n-The username must contain an underscore \n-Should not be longer than 5 characters.",
+                "Username Requirements", JOptionPane.INFORMATION_MESSAGE);
+     
+     Login validUsername = new Login (Username1, Password1);
+          
+     if(validUsername.checkUserName(Username1)==true){
+     JOptionPane.showMessageDialog(null, "Username successfully captured", "Username valid", JOptionPane.PLAIN_MESSAGE);
+     }
+     else if(validUsername.checkUserName(Username1)==false){
+     JOptionPane.showMessageDialog(null, "Username is not correctly formatted, please ensure that "
+             + "your username contains an underscore and is no more than 5 characters in length.",
+             "Username not valid", JOptionPane.ERROR_MESSAGE);
+     }
+    }
+          
+    public static void createPassword(){
+        
+        String Username2 = "";
+        String Password2;
+                    
+     Password2 = JOptionPane.showInputDialog(null, "Create a password"
+           + "\n-Must be at least 8 Characters long"
+           + "\n-Contain a capital letter"
+           + "\n-Contain a number"
+           + "\n-Contain a special character"
+           , "Password Requirements",JOptionPane.INFORMATION_MESSAGE);//Password requirements
+     
+    Login validPassword =  new Login(Username2, Password2);
+             
+     if(validPassword.checkPasswordComplexity(Password2)==true){
+     JOptionPane.showMessageDialog(null, "Password successfully captured", "Password valid", JOptionPane.PLAIN_MESSAGE);
+     
+     }
+     else if(validPassword.checkPasswordComplexity(Password2)==false){
+      JOptionPane.showMessageDialog(null, "Password is not correctly formatted, please ensure that the password"
+              + " contains at least 8 characters, a capital letter, a number and a special character.", "Password not valid", JOptionPane.ERROR_MESSAGE);
+         
+     }     
+}
+}
