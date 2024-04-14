56039a7eee2752a66c545bc6407a555ecd232cc8
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-04-14 15:59:23 +0200

    Fixing my register user method
    Retrying to fix my register user method

--- src/main/java/com/mycompany/prog5121_part1/Login.java
+++ src/main/java/com/mycompany/prog5121_part1/Login.java
@@ -1,96 +1,102 @@
 package com.mycompany.prog5121_part1;
 
 public class Login {
-   private final String Username1;//Declaring the necessary String variables
-   private final String Password1;
-   private String Registration;
+    
+   private String Username1;//Declaring the necessary String variables
+   private String Password1;
    private String strUsername;
    private String strPassword;
-   private String Name;
-   private String Surname;
-   private final String firstName1;
-   private final String lastName1;
-   
-   private boolean validPassword; //Declaring Necessary boolean variables
+   private String strName;
+   private String strSurname;
+   private boolean validPassword;
    private boolean validUsername;
-   
-    public Login(){
-    
-    this.firstName1 = Name;
-    this.lastName1 = Surname;
-    this.Username1 = strUsername;   
-    this.Password1 = strPassword;
-   
-    }
-   
-     public boolean checkUserName(String strUsername){
-   
-    validUsername= false;//Username before user input has been assign to false by default
+   private boolean Registration;
+
+    public Login(String Username1, String Password1, String strName, String strSurname) {
         
-    if(strUsername.contains("_")&& strUsername.length()<=5 ){//The condition states that the username has to have an underscore and is not more than 5 characters long
+        this.strName = strName;
+        this.strSurname = strSurname;
+        this.Username1 = strUsername;
+        this.Password1 = strPassword;
+        Registration = false;
+    }
+
+     public void setfirstName(String strName) {
+         
+        this.strName = strName;
+    }
+
+    public void setlastName(String strSurname) {
+        
+        this.strSurname = strSurname;
+    }
     
-    validUsername = true;//If user inputs their username according to the condition it will be true
+    public String getFirstName(){
+      
+        return strName;
+    }
+    
+    public String getLastName(){
+    
+        return strSurname;
+    
+    }
+
+     public boolean checkUserName(String strUsername){
+       
+    if(strUsername.contains("_")&& strUsername.length()<=5 ){//The condition states that the username has to have an underscore and is not more than 5 characters long
+   
+        return true;//Returns the whether the conditions have been met
     } 
-    return validUsername;//Returns the whether the conditions have been met 
+     return false;
 }    
 
     public boolean checkPasswordComplexity(String strPassword){
-    validPassword = false;//Password before user input is assigned to false by default
            
-    if(strPassword.length()>=8 && strPassword.matches(".*[A-Z].*") && strPassword.matches(".*[0-9].*")&&
-            strPassword.matches(".*[!,.,&,*,?,@,#,%,$].*")) //If statement checks if password entered meets the requirements
-    {
-    validPassword = true;//If user inputs their password according to the condition it will be true
-    }  
-    return validPassword;
-    }
-    
-    public String registerUser(){  
+    if(strPassword.length()>=8 
+      && strPassword.matches(".*[A-Z].*") 
+      && strPassword.matches(".*[0-9].*")
+      && strPassword.matches(".*[!,.,&,*,?,@,#,%,$].*")) //If statement checks if password entered meets the requirements
+    { 
+        return true;
+    }else{
         
-      if(validUsername){
-          
-       return "Username successfully captured";   
-      }
+       return false;
+    }
+   }
+   
+   public String registerUser(){ 
+       
       
-      else if(!validUsername){
-       return """
-              The username has been incorrectly formatted, please ensure that your Username has the following criteria: 
-              -The username should contain an underscore
-              -And it should not be longer than 5 characters""";//If the username is not correct it will return this statement
-    }
-      else if(validPassword){
-          
-       return "Password successfully captured";   
-      }
-        
-    else if(!validPassword){
-    return """
-           The password is not correctly formatted, please ensure that your password has the following criteria: 
-           -The password should contain at least 8 characters
-           -A capital letter
-           -A number
-           -And a special character""";//If the password is not correct it will return this statement
-      
-    }
-     else{ 
-        return "Error";//If none of the conditions are met this statement will return
-    }
-    }  
+     if(){
+         
+     }
+         
+         
+       
+}
+
+
  
     public boolean loginUser(String Username, String Password){
-       return Username.equals(strUsername) && Password.equals(strPassword); //This if statement ensures that the login details entered matches the login in detauls stored when the user re
-       //If login status matches it returns a true
-       //If login status doesnt match it returns a false
+       if(Username.equals(this.Username1) && Password.equals(this.Password1)){; //This if statement ensures that the login details entered matches the login in detauls stored when the user re
+      
+       return true;
+       }
+       else{
+             return false;  
+       }
     
-  }
        
-    public String returnLoginStatus(boolean Username, boolean Password){
-     if(validPassword == true && validUsername == true){//This if statement checks if the information that the user gave is correct
-     return "Welcome " + Name + " " + Surname + " ,it is great to see you again.";//this statement will return if the login is correct
-     }
-     else{
-         return "Username or password is incorrect, please try again";//If the login in is incorrect it will print this statement
-     }  
-    }
+  }
     
+    
+    public String returnLoginStatus(String Username1, String Password1){
+     if (Username1.equals(this.strUsername) && Password1.equals(this.Password1)) {
+        return "Welcome ";
+        }
+     else{
+         return "Username or password is incorrect, please try again";
+        }
+    }
 }
--- src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
+++ src/main/java/com/mycompany/prog5121_part1/PROG5121_Part1.java
@@ -3,142 +3,156 @@ package com.mycompany.prog5121_part1;
 import javax.swing.JOptionPane;
 
 public class PROG5121_Part1 {
-    
+            
     public static void main(String[] args) {   
         
-      int iUserResponse = JOptionPane.showConfirmDialog(null, "Would you like to create an account?" , "Confirm",
+    int iUserResponse = JOptionPane.showConfirmDialog(null, "Would you like to create an account?" , "Confirm",
                 JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);//Allow user to choose whether they want to create an account or not
         
-       if(iUserResponse == JOptionPane.YES_OPTION){
+    if(iUserResponse == JOptionPane.YES_OPTION){
+       String firstName1 = "";
+       String lastName1 = "";
+       Name(firstName1, lastName1);
+       String Username1 = "";
+       createUsername(Username1);
+       String Password1 = "";
+       createPassword(Password1); 
+       successfulLogin(Username1, Password1, firstName1, lastName1);
+       LoginStatus(Username1, Password1, firstName1, lastName1);
 
-     Name(); //Calling this method within the main method
-     createUsername();
-     createPassword();
-     successfulLogin(); 
-     }
-      else if(iUserResponse == JOptionPane.NO_OPTION)
-     {
+    }
+    else if(iUserResponse == JOptionPane.NO_OPTION)
+    {
             
      JOptionPane.showMessageDialog(null, "Thank you for using my app.", "Output", JOptionPane.PLAIN_MESSAGE);
       //If they select no thank the user for using my app
            
       System.exit(iUserResponse);//Exits the program without continuing
       
-      }
-     
+    }     
     }
-     public static void Name(){
-         String firstName = ""; 
-         String lastName = "";
+    
+     public static void Name(String firstName1, String lastName1){
+         String Username1 = "";
+         String Password1 = "";
          
-         while(firstName.isEmpty() || lastName.isEmpty()){
-         firstName = JOptionPane.showInputDialog(null, "Enter your first name: ");
-         lastName = JOptionPane.showInputDialog(null, "Enter your last name: ");
+         while(firstName1.isEmpty() || lastName1.isEmpty()){
+         firstName1 = JOptionPane.showInputDialog(null, "Enter your first name: ");
+         lastName1 = JOptionPane.showInputDialog(null, "Enter your last name: ");
          
-         if(firstName.isBlank()|| lastName.isBlank()){
+         if(firstName1.isBlank()|| lastName1.isBlank()){
              JOptionPane.showMessageDialog(null, "First name and Last name is required",
                      "Retry", JOptionPane.ERROR_MESSAGE);     
          }
-    }
-   }
-     
-    public static void createUsername(){
-    String Username1;
-    String Password1 = "";
-    boolean correctUsername = false; 
-                               
-    Username1 = JOptionPane.showInputDialog(null, "Create a username"
-                + "\n-The username must contain an underscore \n-Should not be longer than 5 characters.",
-                "Username Requirements", JOptionPane.INFORMATION_MESSAGE);
-     
-     Login validUsername = new Login ();
-     
-     while(!correctUsername) {       
-     if(validUsername.checkUserName(Username1)==true){
+        }
          
-     JOptionPane.showMessageDialog(null, validUsername.registerUser(),
-             "Username valid", JOptionPane.PLAIN_MESSAGE);
-     
-     correctUsername = true;
-     }
-    
-     else{
-     JOptionPane.showMessageDialog(null, validUsername.registerUser(),
-             "Username not valid", JOptionPane.ERROR_MESSAGE);
-     
-     Username1 = JOptionPane.showInputDialog(null, "Create a username"
-                + "\n-The username must contain an underscore \n-Should not be longer than 5 characters.",
-                "Username Requirements", JOptionPane.INFORMATION_MESSAGE);
-    
-     validUsername.checkUserName(Username1);
-     }
+         Login getName = new Login(Username1, Password1, firstName1, lastName1);
+         getName.setfirstName(firstName1);
+         getName.setlastName(lastName1);
+         
+     }      
+         
+       public static void createUsername(String Username1) {
+        String Password1 = "";
+        String firstName1 = "";
+        String lastName1 = "";
+        String Username = "";
+        String strName = "";
+        String strSurname = "";
+        boolean Registration = false;
+
+        while(!Registration){
+        
+       Username1 = JOptionPane.showInputDialog(null, "Create a username"
+                    + "\n-The username must contain an underscore"
+                    + "\n-Should not be longer than 5 characters.",
+                    "Username Requirements", JOptionPane.INFORMATION_MESSAGE);
+       
+       Login validUsername = new Login(Username1, Password1, "", "");
+       
+        if(validUsername.checkUserName(Username1)){
+            
+            JOptionPane.showMessageDialog(null, "Username captured successfully", "Username valid", JOptionPane.PLAIN_MESSAGE);
+            Registration = true;
+    }else{
+        JOptionPane.showMessageDialog(null, "Username is not correctly formatted, "
+                + "please ensure that your username meets the following criteria: "
+                + "\n-The username should contain an underscore"
+                + "\n-The username should also not be longer than 5 characters long", "Username is invalid", JOptionPane.ERROR_MESSAGE);
     }
-   }        
-     
-    public static void createPassword(){
+    }
+       }
+       
+    public static void createPassword(String Password1){
         
         String Username1 = "";
-        String Password1;
-        boolean correctPassword = false;
-                    
-     Password1 = JOptionPane.showInputDialog(null, "Create a password"
+        String firstName1= "";
+        String lastName1= "";
+        boolean Registration = false;
+ 
+     while(!Registration){ 
+         
+         Password1 = JOptionPane.showInputDialog(null, "Create a password"
            + "\n-Must be at least 8 Characters long"
            + "\n-Contain a capital letter"
            + "\n-Contain a number"
            + "\n-Contain a special character"
            , "Password Requirements",JOptionPane.INFORMATION_MESSAGE);//Password requirements
-     
-    Login validPassword =  new Login();
-    
-     while(!correctPassword){       
          
-     if(validPassword.checkPasswordComplexity(Password1)==true){
-     JOptionPane.showMessageDialog(null, validPassword.registerUser(), 
+          Login validPassword =  new Login(Username1, Password1, firstName1, lastName1);
+          
+     if(validPassword.checkPasswordComplexity(Password1)){
+     JOptionPane.showMessageDialog(null, "Password captured sucessfully", 
              "Password valid", JOptionPane.PLAIN_MESSAGE);
 
-    correctPassword = true;
+     Registration = true;
      }
      else{
-      JOptionPane.showMessageDialog(null, validPassword.registerUser(), 
+      JOptionPane.showMessageDialog(null, "Password is not correctly formatted,"
+             + " please ensure that your password meet the following criteria:"
+             + "\n-The password should contain at least 8 characters"
+             + "\n-A capital letter"
+             + "\n-A number"
+             + "\n-And a special character", 
               "Password not valid", JOptionPane.ERROR_MESSAGE);
-      
-      Password1 = JOptionPane.showInputDialog(null, "Create a password"
-           + "\n-Must be at least 8 Characters long"
-           + "\n-Contain a capital letter"
-           + "\n-Contain a number"
-           + "\n-Contain a special character"
-           , "Password Requirements",JOptionPane.INFORMATION_MESSAGE);//Password requirements
-      
-      validPassword.checkPasswordComplexity(Password1);
-         
-     }     
-   }
+       }     
+     }
     }
     
-    public static void successfulLogin(){
-        String Username1 = "";
-        String Password1 = ""; 
+    public static void successfulLogin(String Username1, String Password1, String firstName1, String lastName1) {
+        Login Registered = new Login(Username1, Password1, firstName1, lastName1);
         
-        Login Registered = new Login(); 
+        int Login = JOptionPane.showConfirmDialog(null, "Would you like to sign into your account", "Confirm",
+                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        
+         if(Login==JOptionPane.YES_OPTION){
+        Username1 = JOptionPane.showInputDialog(null, "Enter your username:", "Login", JOptionPane.PLAIN_MESSAGE);
+        Password1 =JOptionPane.showInputDialog(null, "Enter your password:", "Login", JOptionPane.PLAIN_MESSAGE);
         
         if(Registered.loginUser(Username1, Password1)){
             
-        JOptionPane.showMessageDialog(null, Registered.registerUser(), 
-                "Account created successfully", JOptionPane.PLAIN_MESSAGE);
-        }
+         JOptionPane.showMessageDialog(null, Registered.registerUser(), "Login valid", JOptionPane.PLAIN_MESSAGE);      
+        } 
+        else{       
+         
+         JOptionPane.showMessageDialog(null, Registered.registerUser(), "Failed", JOptionPane.ERROR_MESSAGE);
+        }      
+    }
+        else if(Login==JOptionPane.NO_OPTION);{
+        JOptionPane.showMessageDialog(null, "Goodbye!", "Output", JOptionPane.PLAIN_MESSAGE);
+        
+        System.exit(Login);
+    }
+}
+
+    
+    public static void LoginStatus(String Username1, String Password1, String firstName1, String lastName1) {
+        Login Status = new Login(Username1, Password1, firstName1, lastName1);
+   
+      String statusMessage= Status.returnLoginStatus(Username1, Password1);
+      
+      JOptionPane.showMessageDialog(null, statusMessage + Status.getFirstName() + " " + Status.getLastName()+ ", great to see you again!"
+               ,"Welcome", JOptionPane.PLAIN_MESSAGE);
         
 }
-    
-    /*public static void LoginStatus(){
-        
-        Login Status = new Login ();
-        if()
-        {
-            JOptionPane.showMessageDialog(null, Status.returnLoginStatus(), "Successfully logged in", 0);
-        }
-    
-        else {
-            
-}
-}*/
 }
