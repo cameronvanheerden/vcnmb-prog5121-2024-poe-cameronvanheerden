5d908ba8702b61af82cafdbff4a0607bdf767682
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-05-13 09:57:09 +0200

    Testing
    Test code

--- src/main/java/com/mycompany/prog5121_part1/PROG5121_POE_Part2.java
+++ src/main/java/com/mycompany/prog5121_part1/PROG5121_POE_Part2.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.prog5121_part1;
 
-/**
- *
- * @author vivia
- */
 public class PROG5121_POE_Part2 {
     
     
-public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
         
         System.out.println();
