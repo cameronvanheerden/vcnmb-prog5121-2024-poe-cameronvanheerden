fe3eb27e311f44de21f8b9bca298f76f36d07037
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-05-14 17:13:58 +0200

    Starting off code
    Added the first and last name to introduce user to the task feature

--- src/main/java/com/mycompany/prog5121_part1/PROG5121_POE_Part2.java
+++ src/main/java/com/mycompany/prog5121_part1/PROG5121_POE_Part2.java
@@ -1,13 +1,27 @@
 package com.mycompany.prog5121_part1;
 
+import javax.swing.JOptionPane;
+
 public class PROG5121_POE_Part2 {
     
     
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         
-        System.out.println();
+        String firstName = "";
+        String lastName = "";
+        
+        JOptionPane.showMessageDialog(null, "Welcome to EasyKanban.", "Welcome ", JOptionPane.PLAIN_MESSAGE);
+        
+        while(firstName.isBlank()||lastName.isBlank()){
+        firstName = JOptionPane.showInputDialog(null, "Please enter your name:", "Sign up", JOptionPane.PLAIN_MESSAGE);
+        lastName = JOptionPane.showInputDialog(null, "Please enter your last name: ", "Sign up", JOptionPane.PLAIN_MESSAGE);
+        
+        if(firstName.isEmpty())
+            JOptionPane.showMessageDialog(null, "First name is required", "Error", JOptionPane.ERROR_MESSAGE);
+        
+        if(lastName.isEmpty())  
+           JOptionPane.showMessageDialog(null, "Last name is required", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
-    
 
