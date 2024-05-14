506f6e2508e7bb94f4d176690c20c38f4b3b4a15
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-05-14 19:35:43 +0200

    Created methods
    Created methods that display in the main method going to work on the numeric menu option now

--- src/main/java/com/mycompany/prog5121_part1/PROG5121_POE_Part2.java
+++ src/main/java/com/mycompany/prog5121_part1/PROG5121_POE_Part2.java
@@ -3,17 +3,34 @@ package com.mycompany.prog5121_part1;
 import javax.swing.JOptionPane;
 
 public class PROG5121_POE_Part2 {
-    
-    
+       static String firstName = "";
+       static String lastName = "";
+       static String menuMessage = "Please select an option:\n"
+                               + "1.) Add Tasks\n"
+                               + "2.) Show Report\n"
+                               + "3.) Quit\n";
+       
     public static void main(String[] args) {
         
-        String firstName = "";
-        String lastName = "";
+        displayWelcome();
+        displayName();  
+        JOptionPane.showMessageDialog(null, "Welcome, "+ firstName + " " + lastName+ ", to the adding Tasks feature!" , 
+                "Task feature", JOptionPane.PLAIN_MESSAGE);
+        displayMenu();
         
-        JOptionPane.showMessageDialog(null, "Welcome to EasyKanban.", "Welcome ", JOptionPane.PLAIN_MESSAGE);
         
+    }
+    
+    private static void displayWelcome(){
+        
+       JOptionPane.showMessageDialog(null, "Welcome to EasyKanban.", "Welcome", JOptionPane.PLAIN_MESSAGE); 
+       
+    }
+    
+    private static void displayName(){
+              
         while(firstName.isBlank()||lastName.isBlank()){
-        firstName = JOptionPane.showInputDialog(null, "Please enter your name:", "Sign up", JOptionPane.PLAIN_MESSAGE);
+        firstName = JOptionPane.showInputDialog(null, "Please enter your first name:", "Sign up", JOptionPane.PLAIN_MESSAGE);
         lastName = JOptionPane.showInputDialog(null, "Please enter your last name: ", "Sign up", JOptionPane.PLAIN_MESSAGE);
         
         if(firstName.isEmpty())
@@ -21,7 +38,22 @@ public class PROG5121_POE_Part2 {
         
         if(lastName.isEmpty())  
            JOptionPane.showMessageDialog(null, "Last name is required", "Error", JOptionPane.ERROR_MESSAGE);
-        }
+        }   
     }
+    
+    private static int displayMenu(){
+        
+        String Menu = JOptionPane.showInputDialog(null, menuMessage, "Main Menu", JOptionPane.INFORMATION_MESSAGE);
+        
+        if(Menu==null){
+            
+            return 3;
+         }
+        
+       return Integer.parseInt(Menu);
+    }
+    
+    
+    
 }
 
