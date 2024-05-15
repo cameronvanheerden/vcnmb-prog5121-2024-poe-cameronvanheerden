e808a8f96f1499f15d887769ceb9a1f506902cf7
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-05-15 11:06:35 +0200

    Fixed the main method and added a few other methods
    Added a do while loop and within the do while loop there is a switch statement within the do while loop and the break terminates the case within the loop

--- src/main/java/com/mycompany/prog5121_part1/PROG5121_POE_Part2.java
+++ src/main/java/com/mycompany/prog5121_part1/PROG5121_POE_Part2.java
@@ -6,16 +6,36 @@ public class PROG5121_POE_Part2 {
        static String firstName = "";
        static String lastName = "";
        static int numericMenu;
+       static int numTask;
        
     public static void main(String[] args) {
         
         displayWelcome();
         displayName();  
-        JOptionPane.showMessageDialog(null, "Welcome, "+ firstName + " " + lastName+ ", to the adding Tasks feature!" , 
-                "Task feature", JOptionPane.PLAIN_MESSAGE);
-        displayMenu();
-    
+        do{numericMenu = displayMenu();
+        
+            switch(numericMenu){
+                case 1: 
+                    addTasks();
+                break;
+                
+                case 2: 
+                    showReport();
+                break;
+                
+                case 3:
+                    confirmQuit();
+                break;
+                
+                default:
+                    JOptionPane.showMessageDialog(null, "Invalid choice", "Please select again", JOptionPane.ERROR_MESSAGE);
+                    
+            }
     }
+        while(numericMenu != 3);
+        
+        goodbyeMessage();
+ }
     private static void displayWelcome(){
         
        JOptionPane.showMessageDialog(null, "Welcome to EasyKanban.", "Welcome", JOptionPane.PLAIN_MESSAGE); 
@@ -33,7 +53,10 @@ public class PROG5121_POE_Part2 {
         
         if(lastName.isEmpty())  
            JOptionPane.showMessageDialog(null, "Last name is required", "Error", JOptionPane.ERROR_MESSAGE);
-        }   
+        }
+        
+        JOptionPane.showMessageDialog(null, "Welcome, "+ firstName + " " + lastName+ ", to the adding Tasks feature!" , 
+                "Task feature", JOptionPane.PLAIN_MESSAGE);
     }
     
     
@@ -44,4 +67,42 @@ public class PROG5121_POE_Part2 {
                    JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]) +1;
         
 }
+    private static void addTasks(){
+        
+        numTask = Integer.parseInt(JOptionPane.showInputDialog(null, "How many tasks would you like to create",
+                   "Input", JOptionPane.PLAIN_MESSAGE));
+       /* Task[] tasks = new Task[numTask];
+        
+        for(){
+            
+        } */       
+    }
+    
+    private static void showReport(){
+        JOptionPane.showMessageDialog(null, "Coming soon", "Report", JOptionPane.INFORMATION_MESSAGE);
+        
+        goodbyeMessage();
+        System.exit(0);
+    }
+    
+    private static void confirmQuit(){
+        
+        int quit = JOptionPane.showConfirmDialog(null, "Are you sure you would like to exit the program?",
+                "Confirm Quit", JOptionPane.YES_NO_OPTION);
+        
+        if(quit==JOptionPane.YES_OPTION) {
+            
+            goodbyeMessage();
+            System.exit(0);
+        }
+        
+        if(quit==JOptionPane.NO_OPTION)
+            displayMenu();
+            
+    }
+    
+    private static void goodbyeMessage(){
+        
+        JOptionPane.showMessageDialog(null, "Thank you for using my adding Tasks feature program", "Goodbye", JOptionPane.PLAIN_MESSAGE);
+    }
 }
