3b52543b1d284d4d96dcd7ab97c4c67004cd503e
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-05-23 22:34:01 +0200

    Unit test class created

---
+++ src/test/java/PROG5121_Part2_UnitTesting.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PROG5121_Part2_UnitTesting {
+    
+    public PROG5121_Part2_UnitTesting() {
+    }
+    
+   
+}
