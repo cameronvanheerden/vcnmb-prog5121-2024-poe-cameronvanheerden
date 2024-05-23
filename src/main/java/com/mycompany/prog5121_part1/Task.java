4ecaaa6909171288a329aa220ca6ce9687340b9d
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-05-23 21:59:05 +0200

    Fixed my task class
    Testing to see if the task class would return the correct messages when i call it back to the main method

--- src/main/java/com/mycompany/prog5121_part1/Task.java
+++ src/main/java/com/mycompany/prog5121_part1/Task.java
@@ -1,35 +1,28 @@
 package com.mycompany.prog5121_part1;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Task {
     
-    private String taskID ="";
-    private String taskName = "";
-    private int taskNumber = 0;
-    private String developerName ="";
+    private String taskID;
+    private String taskName;
+    private int taskNumber;
+    private String developerName;
     private int taskDuration;
     private String taskDescription;
-    private String statusOptions[]= {"To do", "Done", "Doing"};
-    private int choice;
-    private String taskStatus ="";
-    private int totalHours = 0;
-    static List<Task> tasks = new ArrayList();
+    private String taskStatus;
 
-    public Task(){
+   
+    public Task(int taskNumber, String taskName, String taskDescription, String developerName, int taskDuration, String taskStatus, String taskID){
         
-        this.taskStatus= taskStatus;
-        this.developerName = developerName;
-        this.taskNumber = taskNumber;
+        this.taskNumber= taskNumber;
         this.taskName = taskName;
-        this.taskDescription = taskDescription;
-        this.taskID = createTaskID();
+        this.taskDescription= taskDescription;
+        this.developerName = developerName;
         this.taskDuration = taskDuration;
-        if(choice >= 0 && choice < statusOptions.length ){
-          
-            this.choice=choice;
-        }
+        this.taskStatus = taskStatus;
+        this.taskID = taskID;
+              
     }   
     
     public boolean checkTaskDescription(){
@@ -38,33 +31,33 @@ public class Task {
     }
     
     public String createTaskID(){
-
-        String firstTwoLetters = taskName.substring(0, 2).toUpperCase();
-        
-        String lastThreeLetters = developerName.substring(developerName.length() - 3).toUpperCase();
-        
-        taskID = firstTwoLetters + ":" + (taskNumber + 1) + ":"+ lastThreeLetters;
-             
-        return taskID;
+       
+        return taskName.substring(0 , 2).toUpperCase() + taskNumber + developerName.substring(developerName.length() - 3).toUpperCase();
              
     }
     
     public String printTaskDetails(){
         
-        return "Task Status: " + statusOptions[choice]
+        return "Task Status: " + taskStatus
                + "\nDeveloper Details: " + developerName
-               + "\nTask Number: "+ taskNumber++
+               + "\nTask Number: "+ taskNumber
                + "\nTask Name: "+ taskName 
                + "\nTask Descrpition: "+taskDescription
                + "\nTask ID: " + taskID
                + "\nTask Duration: " + taskDuration + " hours";
     } 
     
-    public int returnTotalHours(){
+    public int gettaskDuration(){
+        
+        return taskDuration;
+    }
+    
+    public static int returnTotalHours(List <Task> tasks){
+        int totalHours = 0;
               
         for(Task task: tasks){
      
-            totalHours += task.taskDuration;
+            totalHours += task.gettaskDuration();
         }
         return totalHours;
               
