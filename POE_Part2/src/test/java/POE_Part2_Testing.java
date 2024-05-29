4d24ceaebc97c13df641519fd28ce42ccbc073cd
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-05-29 09:52:05 +0200

    Restarted code
    I had to restart my code because my automated test wasnt working and i needed to restart

---
+++ POE_Part2/src/test/java/POE_Part2_Testing.java
@@ -0,0 +1,62 @@
+
+import com.mycompany.poe_part2.Task;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class POE_Part2_Testing {
+    
+    @Test
+    void testCheckTaskDescription_Success(){
+    
+        Task testSuccess = new Task (1, "Login feature", "Create Login to authenticate users", "Robyn Harrison", 8,"To do", "" );    
+        
+        boolean actual = testSuccess.checkTaskDescription();
+        boolean expected = true;
+    
+        assertEquals(expected, actual, "Task description expected to be valid");
+        
+    }
+    
+    @Test
+    void testCheckTaskDescription_Failure(){
+        
+        Task testFailed = new Task(1, "Login feature", "The task description is intentionally going to be longer than 50 character",
+                "Robyn Harrison", 8, "To do", "");
+        
+        boolean actual = testFailed.checkTaskDescription();
+        boolean expected = false;
+        
+        assertEquals(expected, actual, "Task description expected to be invalid");
+        
+    }
+    
+    @Test
+    void testCreateTaskID(){
+  
+        Task testID = new Task(1, "Login feature", "Create Login to authenticate users", "Robyn Harrison", 8, "To do", "");
+        
+        
+        String actual = testID.createTaskID();
+        String expected = "LO:1:SON";
+        
+        assertEquals(expected, actual, "Test case 1 failed");
+      
+    }
+    
+    @Test
+    void testReturnTotalHours(){
+        
+        List <Task> totalHours = new ArrayList<>();
+        
+        totalHours.add(new Task(1, "Login feature", "Create login to authenicate users","Robyn Harrison", 8, "To do", "" ));
+        totalHours.add(new Task(2, "Add Task feature", "Create Add Task feature to add task users", "Mike Smith", 10, "Doing", ""));
+        
+        int actual = Task.returnTotalHours(totalHours);
+        int expected = 18;
+        
+        assertEquals(expected, actual , "Total Hours should be 18");       
+    }
+}
