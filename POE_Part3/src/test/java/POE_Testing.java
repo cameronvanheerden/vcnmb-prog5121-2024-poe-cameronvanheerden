9a005ed0b2eb043e8e41d09654cf7ae08322be4b
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-06-12 16:03:42 +0200

    Test 3,4,5 complete
    I followed the steps i did in the main class and completed the 3 tests

--- POE_Part3/src/test/java/POE_Testing.java
+++ POE_Part3/src/test/java/POE_Testing.java
@@ -39,12 +39,63 @@ public class POE_Testing {
         assertEquals(11, (int )POE_Part3.taskDurationArray.get(maxDurationIndex));
     }
     
+    @Test
     public void testSearchByName(){
         
         String taskName = "Create Login";
+        boolean found = false;
         
+        for(int i = 0; i < POE_Part3.taskNameArray.size(); i++){
+            
+            if(POE_Part3.taskNameArray.get(i).equalsIgnoreCase(taskName)){
+                
+               assertEquals("Mike Smith", POE_Part3.developerArray.get(i)); 
+               assertEquals("Create Login", POE_Part3.taskNameArray.get(i));
+                               
+                found = true;
+                break;
+            }
+         }
         
+        assertTrue(found);
+    }
+    
+    @Test
+    public void testTaskAssignedToDeveloper(){
         
+        String developerName = "Samantha Paulson";
+        StringBuilder searchByDeveloper = new StringBuilder();
+        
+        for(int i = 0; i < POE_Part3.developerArray.size(); i++){
+            
+            if(POE_Part3.developerArray.get(i).equalsIgnoreCase(developerName)){
+                
+                searchByDeveloper.append(POE_Part3.taskNameArray.get(i));
+            }
+        }
+        assertEquals("Create Reports", searchByDeveloper.toString());
+    }
+    
+    @Test
+    public void testDeleteTaskByName(){
+        
+        String taskName = "Create Reports";
+        boolean found = false;
+        
+        for(int i = 0; i < POE_Part3.taskNameArray.size(); i++){
+            
+            if(POE_Part3.taskNameArray.get(i).equalsIgnoreCase(taskName)){
+                POE_Part3.developerArray.remove(i);
+                POE_Part3.taskNameArray.remove(i);
+                POE_Part3.taskIDArray.remove(i);
+                POE_Part3.taskDurationArray.remove(i);
+                POE_Part3.taskStatusArray.remove(i);
+                found = true;
+                break;
+            }
+        }
+        assertTrue(found);
+        assertFalse(POE_Part3.taskNameArray.contains(taskName));
     }
 
 }
