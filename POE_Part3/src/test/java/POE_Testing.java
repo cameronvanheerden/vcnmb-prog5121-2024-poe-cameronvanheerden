83fed0e05729f84a226fa7406fe1ac5d16a9eda6
cameronvanheerden <ST10435031@vcconnect.edu.za>
2024-06-12 15:38:21 +0200

    Test two complete
    Test for longest duration complete and the system returns the developer and the hours

--- POE_Part3/src/test/java/POE_Testing.java
+++ POE_Part3/src/test/java/POE_Testing.java
@@ -21,5 +21,22 @@ public class POE_Testing {
         assertEquals("Glenda Oberholzer", POE_Part3.developerArray.get(3));
         
     }
+    
+    @Test
+    public void testLongestDurationArray(){
+        
+        int maxDurationIndex = 0;
+        
+        for(int i = 1; i < POE_Part3.taskDurationArray.size(); i++){
+            
+            if(POE_Part3.taskDurationArray.get(i) > POE_Part3.taskDurationArray.get(maxDurationIndex)){
+             
+                maxDurationIndex = i;
+            }
+        }
+        
+        assertEquals("Glenda Oberholzer", POE_Part3.developerArray.get(maxDurationIndex));
+        assertEquals(11, (int )POE_Part3.taskDurationArray.get(maxDurationIndex));
+    }
 
 }
